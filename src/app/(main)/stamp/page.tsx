3fbb3d642ebd481103fdd032ed78428020d587fa
なえざつく <naezatsuku653@gmail.com>
2026-05-16 23:57:50 +0900

    エラー直した

--- src/app/(main)/stamp/page.tsx
+++ src/app/(main)/stamp/page.tsx
@@ -218,7 +218,10 @@ export default function StampPage() {
   useEffect(() => {
     let id = localStorage.getItem('stamp_user_id')
     if (!id) {
-      id = crypto.randomUUID()
+      id = (crypto.randomUUID?.() ?? 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        const r = Math.random() * 16 | 0
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+      }))
       localStorage.setItem('stamp_user_id', id)
     }
     setUserId(id)
